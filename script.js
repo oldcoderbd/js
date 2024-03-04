17f36c1b5802ac79efc5d79cd4b22c8e7626f6b2
oldcoderbd <oldcoderbd@gmail.com>
2024-03-04 08:09:15 +0600

    ok

---
+++ script.js
@@ -0,0 +1,103 @@
+//alert('Sabdhan hoye jao Pollob');
+
+/* let Name = 'Sajid Ahmed';
+
+console.log(Name)
+
+let Name = 'Sajid Ahmed';
+
+console.log(Name)
+
+let a = 10
+if(a<15){
+    console.log('a is less than 10');
+}
+else{
+    console.log('a is equal to 10')
+}
+else if(a>5){
+    console.log('a is greater than 5')
+}
+
+let a = '16', b=20
+if(a===b){
+    console.table('a is a number')
+}
+else{
+    console.table('is a string')
+}
+if(typeof a == 'string'){
+    console.log(typeof a)
+}
+
+let mark = 53;
+
+switch(true){
+    case (mark>=33 && mark<40): console.log('E');
+    break;
+    case (mark>40 && mark<50): console.log('D');
+    break;
+    case (mark>50 && mark<60): console.log('C');
+    break;
+    case (mark>60 && mark<70): console.log('B');
+    break;
+    case (mark>70 && mark<80): console.log('A');
+    break;
+    case (mark>80 && mark<100): console.log('A+');
+    break;
+    default:console.log('He/She is Fail');
+}
+
+for(let odd=1; odd<100; odd=odd+2){
+    console.log('here ' + odd) ;
+}
+
+let a = 5;
+
+while(a*a < 1678){
+    console.log('Here is ${a=a*a}');
+}
+
+let x = 50;
+
+function checkOddEven(x){
+    if(x/2>=60){
+        console.log('x greater than 50');
+    }
+    else console.log('x is lower than 50')
+}
+checkOddEven(x);
+
+function multi(a, b){
+    console.log(a*b)
+}
+multi(3, 5);
+
+let sum = 1;
+function div(a, b){
+    for(a; a<b; a++){
+        sum = a+sum;
+    }
+    console.log(sum/3);
+}
+div(10, 20);*/
+
+function sajid(a, b, c){
+    
+}
+sajid(66, 88, 77);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
